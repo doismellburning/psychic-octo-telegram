ee97c4e6b1de1e2ba17ecf35e23f1ba9d21dc9a5
Kristian Glass <git@doismellburning.co.uk>
2022-08-24 14:54:22 +0100

    Initial PoC

---
+++ README.md
@@ -0,0 +1,5 @@
+# psychic-octo-telegram
+
+My quick and dirty experiments with the MQTT feed from https://pskreporter.info/
+
+With many thanks to [Tom M0LTE](https://m0lte.uk/) for setting the feed up.
---
+++ pot.js
@@ -0,0 +1,41 @@
+//const mqtt = require('mqtt')
+
+const options = {
+  // Clean session
+  clean: true,
+  connectTimeout: 4000,
+  // Auth
+  //clientId: 'mqtt.pskreporter.info',
+  //username: 'emqx_test',
+  //password: 'emqx_test',
+}
+
+const HOST = "ws://mqtt.pskreporter.info:81"
+
+const client  = mqtt.connect(HOST, options)
+
+const outputLogId = "outputLog"
+const outputLog = document.createElement("div")
+outputLog.id = outputLogId
+document.body.appendChild(outputLog)
+
+client.on('connect', function () {
+  console.log('Connected')
+
+  client.subscribe('pskr/rx/2E0KGG', function (err) {
+  //client.subscribe('pskr/firehose', function (err) {
+    if (err) {
+      console.log(err)
+      return
+    } else {
+      console.log("Subscribed!")
+      client.on('message', function(topic, payload, packet) {
+        console.log(`Topic: ${topic}, Message: ${payload.toString()}, Packet: ${packet}`)
+		var message = JSON.parse(payload.toString())
+        var entry = document.createElement("div")
+        entry.innerHTML = `${message['senderCallsign']} @ ${message['senderLocator']} (${message['senderCountryName']})`
+        outputLog.appendChild(entry)
+      })
+    }
+  })
+})
